a1983af61b4ce4cd7619d12658ac23de48d30fcb
Trent Ellingsen <trentellingsen@gmail.com>
2012-10-12 05:23:36 -0700

    added logo, links, and styling
    booya for github dynamically changing!

---
+++ js/github.js
@@ -0,0 +1,42 @@
+var GitHub = {
+	size: 0,
+	links: new Array()
+};
+
+function Project(gitObj) {
+	this.id = GitHub.size++;
+	this.url = gitObj.html_url;
+	this.name = gitObj.name;
+	this.description = gitObj.description;
+	this.lastUpdated = gitObj.pushed_at;
+};
+
+GitHub.addRecentProjectsToSelector = function(selector) {
+	$.each(GitHub.links, function(index, link) {
+		var prevMonth = new Date();
+		prevMonth.setMonth(prevMonth.getMonth() - 1);
+		
+		var recent = (new Date(link.lastUpdated) > prevMonth);
+		if (recent) {
+			$(selector).append('<a href="' + link.url + '" title="' + link.description + '">' + link.name + '</a>');
+		}
+	});
+};
+
+GitHub.setup = function() {
+	GitHub.setupPublicRepoList();
+};
+
+GitHub.setupPublicRepoList = function() {
+	var url = "https://api.github.com/users/guinto/repos";
+	$.ajax({
+		url: url,
+		dataType: "jsonp",
+		success: function(response) {
+			var data = response.data; 
+			for (var i = 0; i < data.length; i++) {
+				GitHub.links.push(new Project(data[i]));
+			}
+		}
+	});
+};

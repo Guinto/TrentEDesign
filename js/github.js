957795bd127b95d0578ef3fe61ec90234229701b
Trent Ellingsen <trentellingsen@gmail.com>
2013-01-08 22:47:07 -0800

    made many changes
    Got it to read local .md files
    Also did more stylings

--- js/github.js
+++ js/github.js
@@ -1,6 +1,6 @@
 var GitHub = {
-	size: 0,
-	links: new Array()
+	size : 0,
+	links : new Array()
 };
 
 function Project(gitObj) {
@@ -15,10 +15,12 @@ GitHub.addRecentProjectsToSelector = function(selector) {
 	$.each(GitHub.links, function(index, link) {
 		var prevMonth = new Date();
 		prevMonth.setMonth(prevMonth.getMonth() - 1);
-		
+
 		var recent = (new Date(link.lastUpdated) > prevMonth);
 		if (recent) {
-			var $githubLinks = $('<a href="' + link.url + '" title="' + link.description + '">' + link.name + '</a>').hide();
+			var $githubLinks = $(
+					'<a href="' + link.url + '" title="' + link.description
+							+ '">' + link.name + '</a>').hide();
 			$githubLinks.appendTo($(selector)).slideDown();
 		}
 	});
@@ -30,29 +32,73 @@ GitHub.setup = function() {
 
 GitHub.setupPublicRepoList = function() {
 	var url = "https://api.github.com/users/guinto/repos";
+	GitHub.getReadme("TrentEDesign");
 	$.ajax({
-		url: url,
-		dataType: "jsonp",
-		success: function(response) {
+		url : url,
+		dataType : "jsonp",
+		success : function(response) {
 			var data = response.data;
 			console.log(data);
-			for (var i = 0; i < data.length; i++) {
+			for ( var i = 0; i < data.length; i++) {
 				GitHub.links.push(new Project(data[i]));
-				GitHub.getReadme(data[i].id);
 			}
+			// Put the most recent first
+			GitHub.links.reverse();
+			
+			GitHub.getReadme("TrentEDesign");
 		}
 	});
 };
 
 GitHub.getReadme = function(repoID) {
-	var url = "https://api.github.com/repos/guinto/" + repoID + "/readme";
-	console.log(url);
+	$.get(repoID + "README.md", function(data) {
+		$.ajax({
+			url : "http://documentup.com/compiled",
+			dataType : "jsonp",
+			data : {
+				content : data,
+				name : repoID
+			},
+			success : function(resp) {
+				$('#projectTrentEDesign .js-replace').append(resp.html);
+				$('#projectTrentEDesign .js-replace').replaceWith($('#content').removeAttr('id'));
+			}
+		});
+	});
+}
+
+GitHub.getReadme2 = function(repoID) {
+	$.get("README.md", function(data) {
+		console.log(data);
+	});
+
+	//var url = "https://api.github.com/repos/guinto/" + repoID + "/readme";
 	$.ajax({
-		url: url,
-		dataType: "jsonp",
-		success: function(response) {
-			var data = response.data; 
+		url : url,
+		dataType : "jsonp",
+		data : {
+			"Accept" : "application/vnd.github.beta.raw+json"
+		},
+		success : function(response) {
+			var data = response.data;
 			console.log(data);
+			/*$.ajax({
+				url : "http://documentup.com/compiled",
+				dataType : "jsonp",
+				data : {
+					content : README.md,
+					name : "TrentEDesign"
+				},
+				success : function(resp) {
+					// `status` is always provided
+					if (resp.status == 200) {
+						// Write to your document
+						document.open();
+						document.write(resp.html);
+						document.close();
+					}
+				}
+			});*/
 		}
 	});
 };
